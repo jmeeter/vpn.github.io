9d3cbf3e2cf96b8be55692aca840a91ddda69ae6
skarz <40373461+jmeeter@users.noreply.github.com>
2022-12-28 10:45:19 -0800

    site
    site

---
+++ xmlrpc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://new.whoisyourvpn.com/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://new.whoisyourvpn.com/";
+		</script>
+
+		<p>You are being redirected to <a href="https://new.whoisyourvpn.com/">https://new.whoisyourvpn.com/</a></p>
+	</body>
+</html>
